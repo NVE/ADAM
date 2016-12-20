8512227a07425ea22bba93a8ddb820477d6f2552
kmunve <kmu@nve.no>
2016-12-20 15:29:42 +0100

    Got getJSON working in the example

--- adam/load_adam.js
+++ adam/load_adam.js
@@ -2,10 +2,9 @@
  * Created by kmu on 01.11.2016.
  */
 
-function load() {
-    //var adam_def = JSON.parse('adam');
-    $.getJSON("adam.json", function (adam_def) {
-        alert(adam_def.ADAM.SensitivityToTriggers.Unreactive.natural_trigger);
-        adam_def.ADAM.SpatialDistribution.appendTo( "body" );
+function loadAdam() {
+    $.getJSON("adam.json", function (adam_defs) {
+        //alert(adam_def.ADAM.SensitivityToTriggers.Unreactive.natural_trigger);
+        $("<div>" + adam_def.ADAM.SpatialDistribution.Isolated.density+ "</div>").appendTo( "body" );
     });
 }
