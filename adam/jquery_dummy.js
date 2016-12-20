c2dc76afc7e046bc1803437b640476b804f32a3a
kmunve <kmu@nve.no>
2016-12-20 14:06:26 +0100

    Some refactoring and testing

--- adam/jquery_dummy.js
+++ adam/jquery_dummy.js
@@ -8,3 +8,15 @@ $(document).ready(function(){
         $("p").hide();
     });
 });
+
+$.getJSON( "ajax/test.json", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
