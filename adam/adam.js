c88f232c9142d48a16354128037da8445a2efc45
kmunve <kmu@nve.no>
2017-01-13 14:46:05 +0100

    got the function for changing between terminologies working

--- adam/adam.js
+++ adam/adam.js
@@ -6,25 +6,29 @@ var CMAH_def = {
 
     "sensitivity_to_triggers": {
         "label": "Sensitivity to triggers",
-        "unreactive": {
-            "label": "Un-reactive",
-            "natural_trigger": "No avalanches",
-            "human_trigger": "No avalanches"
-        },
-        "stubborn": {
-            "label": "Stubborn",
-            "natural_trigger": "Few",
-            "human_trigger": "Difficult to trigger"
-        },
-        "reactive": {
-            "label": "Reactive",
-            "natural_trigger": "Several",
-            "human_trigger": "Easy to trigger"
-        },
-        "touchy": {
-            "label": "Touchy",
-            "natural_trigger": "Numerous",
-            "human_trigger": " Triggering almost certain"
+        "natural_trigger": "Natural trigger",
+        "human_trigger": "Human trigger",
+        classes: {
+            "unreactive": {
+                "label": "Un-reactive",
+                "natural_trigger": "No avalanches",
+                "human_trigger": "No avalanches"
+            },
+            "stubborn": {
+                "label": "Stubborn",
+                "natural_trigger": "Few",
+                "human_trigger": "Difficult to trigger"
+            },
+            "reactive": {
+                "label": "Reactive",
+                "natural_trigger": "Several",
+                "human_trigger": "Easy to trigger"
+            },
+            "touchy": {
+                "label": "Touchy",
+                "natural_trigger": "Numerous",
+                "human_trigger": " Triggering almost certain"
+            }
         }
     },
 
@@ -50,6 +54,60 @@ var CMAH_def = {
     }
 };
 
+
+var EAWS_def = {
+
+    "sensitivity_to_triggers": {
+        "label": "Sensitivity to triggers",
+        "natural_trigger": "Natural trigger",
+        "human_trigger": "Human trigger",
+        classes: {
+            "unreactive": {
+                "label": "Generally from high additional load",
+                "natural_trigger": "No avalanches",
+                "human_trigger": "No avalanches"
+            },
+            "stubborn": {
+                "label": "Primarily from high additional load",
+                "natural_trigger": "Few",
+                "human_trigger": "Difficult to trigger"
+            },
+            "reactive": {
+                "label": "With low additional load possible",
+                "natural_trigger": "Several",
+                "human_trigger": "Easy to trigger"
+            },
+            "touchy": {
+                "label": "With low additional load probable",
+                "natural_trigger": "Numerous",
+                "human_trigger": " Triggering almost certain"
+            }
+        }
+    },
+
+    "spatial_distribution": {
+        "label": "Spatial distribution",
+        "classes": {
+            "isolated": {
+                "label": "Isolated",
+                "description_1": "The instability is spotty and found in only a few terrain features.",
+                "description_2": "Evidence is rare and hard to find."
+            },
+            "specific": {
+                "label": "Specific",
+                "description_1": "The instability exists in terrain features with common characteristics.",
+                "description_2": "Evidence exists but is not always obvious."
+            },
+            "widespread": {
+                "label": "Widespread",
+                "description_1": "The instability is found in many locations and terrain features.",
+                "description_2": "Evidence is everywhere and easy to find."
+            }
+        }
+    }
+};
+
+
 function setLabels(ADAMlabels) {
     /*
      $("#").text(ADAMlabels.);
@@ -63,23 +121,24 @@ function setLabels(ADAMlabels) {
 
     //Sensitivity to triggers
     $("#sensitivity_l").text(ADAMlabels.sensitivity_to_triggers.label);
-    $("#touchy_l").text(ADAMlabels.sensitivity_to_triggers.touchy.label);
-    $("#reactive_l").text(ADAMlabels.sensitivity_to_triggers.reactive.label);
-    $("#stubborn_l").text(ADAMlabels.sensitivity_to_triggers.stubborn.label);
-    $("#unreactive_l").text(ADAMlabels.sensitivity_to_triggers.unreactive.label);
+    $("#touchy_l").html("<p data-toggle=\"tooltip\" data-placement=\"top\" title=\"Click for definitions\">" + ADAMlabels.sensitivity_to_triggers.classes.touchy.label + "</p>");
+    $("#reactive_l").text(ADAMlabels.sensitivity_to_triggers.classes.reactive.label);
+    $("#stubborn_l").text(ADAMlabels.sensitivity_to_triggers.classes.stubborn.label);
+    $("#unreactive_l").text(ADAMlabels.sensitivity_to_triggers.classes.unreactive.label);
 
 // Setting up the modals
+    //Spatial distribution
     var sdc = ADAMlabels.spatial_distribution.classes;
     var i;
     for (i in sdc) {
 
         $("body").append(
-            "<div id=\"modal" + sdc[i].label + "\" class=\"modal fade\" role=\"dialog\">" +
+            "<div id=\"modal_" + i + "\" class=\"modal fade\" role=\"dialog\">" +
             "<div class=\"modal-dialog\">" +
             "<div class=\"modal-content\">" +
             "<div class=\"modal-header cmah_color\">" +
             "<button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button>" +
-            "<h4 class=\"modal-title\">Spatial Distribution: <u>" + sdc[i].label + "</u></h4>" +
+            "<h4 class=\"modal-title\">" + ADAMlabels.spatial_distribution.label + ": <u>" + sdc[i].label + "</u></h4>" +
             "</div>" +
             "<div class=\"modal-body\">" +
             "<p>" + sdc[i].description_1 + "</p>" +
@@ -91,6 +150,28 @@ function setLabels(ADAMlabels) {
         );
     }
 
+
+    //Spatial distribution
+    var sttc = ADAMlabels.sensitivity_to_triggers.classes;
+    for (i in sttc) {
+
+        $("body").append(
+            "<div id=\"modal_" + i + "\" class=\"modal fade\" role=\"dialog\">" +
+            "<div class=\"modal-dialog\">" +
+            "<div class=\"modal-content\">" +
+            "<div class=\"modal-header cmah_color\">" +
+            "<button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button>" +
+            "<h4 class=\"modal-title\">" + ADAMlabels.sensitivity_to_triggers.label + ": <u>" + sttc[i].label + "</u></h4>" +
+            "</div>" +
+            "<div class=\"modal-body\">" +
+            "<p><b>" + ADAMlabels.sensitivity_to_triggers.natural_trigger + ":</b> " + sttc[i].natural_trigger + "</p>" +
+            "<p><b>" + ADAMlabels.sensitivity_to_triggers.human_trigger + ":</b> " + sttc[i].human_trigger + "</p>" +
+            "</div>" +
+            "</div>" +
+            "</div>" +
+            "</div>"
+        );
+    }
 }
 
 
@@ -100,16 +181,13 @@ $(document).ready(function () {
         setLabels(CMAH_def);
 
         $("#setCMAH").click(function () {
-            ADAMlables = CMAH_def;
-            $("#sensitivity_label").text(ADAMlables.sensitivity_to_triggers.label);
+            setLabels(CMAH_def);
         });
+
         $("#setEAWS").click(function () {
-            ADAMlables = EAWS_def;
-            $("#sensitivity_label").text("Additional loading");
+            setLabels(EAWS_def);
         });
-        $("#btn3").click(function () {
-            $("#test3").val("Dolly Duck");
-        });
+
     }
 );
 
