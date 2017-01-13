1671057575e8cf7d80cc4264e1a1668bc6d58da0
kmunve <kmu@nve.no>
2017-01-13 13:13:10 +0100

    inserting modal creation loop

--- adam/adam.js
+++ adam/adam.js
@@ -2,21 +2,124 @@
  * Created by kmu on 01.11.2016.
  */
 
+var CMAH_def = {
+
+    "sensitivity_to_triggers": {
+        "label": "Sensitivity to triggers",
+        "unreactive": {
+            "label": "Un-reactive",
+            "natural_trigger": "No avalanches",
+            "human_trigger": "No avalanches"
+        },
+        "stubborn": {
+            "label": "Stubborn",
+            "natural_trigger": "Few",
+            "human_trigger": "Difficult to trigger"
+        },
+        "reactive": {
+            "label": "Reactive",
+            "natural_trigger": "Several",
+            "human_trigger": "Easy to trigger"
+        },
+        "touchy": {
+            "label": "Touchy",
+            "natural_trigger": "Numerous",
+            "human_trigger": " Triggering almost certain"
+        }
+    },
+
+    "spatial_distribution": {
+        "label": "Spatial distribution",
+        "isolated": {
+            "label": "Isolated",
+            "density": "The instability is spotty and found in only a few terrain features.",
+            "evidence": "Evidence is rare and hard to find."
+        },
+        "specific": {
+            "label": "Specific",
+            "density": "The instability exists in terrain features with common characteristics.",
+            "evidence": "Evidence exists but is not always obvious."
+        },
+        "widespread": {
+            "label": "Widespread",
+            "description_1": "The instability is found in many locations and terrain features.",
+            "description_2": "Evidence is everywhere and easy to find."
+        }
+    }
+};
+
+function setLabels(ADAMlabels) {
+    /*
+     $("#").text(ADAMlabels.);
+     */
+
+// Setting up ADAM
+    //Spatial distribution
+    $("#widespread_l").text(ADAMlabels.spatial_distribution.widespread.label);
+    $("#specific_l").text(ADAMlabels.spatial_distribution.specific.label);
+    $("#isolated_l").text(ADAMlabels.spatial_distribution.isolated.label);
+
+    //Sensitivity to triggers
+    $("#sensitivity_l").text(ADAMlabels.sensitivity_to_triggers.label);
+    $("#touchy_l").text(ADAMlabels.sensitivity_to_triggers.touchy.label);
+    $("#reactive_l").text(ADAMlabels.sensitivity_to_triggers.reactive.label);
+    $("#stubborn_l").text(ADAMlabels.sensitivity_to_triggers.stubborn.label);
+    $("#unreactive_l").text(ADAMlabels.sensitivity_to_triggers.unreactive.label);
+
+// Setting up the modals
+
+    for (al in ADAMlabels.spatial_distribution) {
+
+
+        $("body").append(
+            "<div id=\"modal" + al.label + "\" class=\"modal fade\" role=\"dialog\">" +
+            "<div class=\"modal-dialog\">" +
+            "<div class=\"modal-content\">" +
+            "<div class=\"modal-header cmah_color\">" +
+            "<button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button>" +
+            "<h4 class=\"modal-title\">Spatial Distribution: <u>" + al.label + "</u></h4>" +
+            "</div>" +
+            "<div class=\"modal-body\">" +
+            "<p>" + al.description_1 + "</p>" +
+            "<p>" + al.description_2 + "</p>" +
+            "</div>" +
+            "</div>" +
+            "</div>" +
+            "</div>"
+        );
+    }
+    ;
+
+}
+
+
 $(document).ready(function () {
-    $("#setCMAH").click(function () {
-        ADAMlables = CMAH_def;
-        $("#sensitivity_label").text(ADAMlables.sensitivity_to_triggers.label);
-    });
-    $("#setEAWS").click(function () {
-        ADAMlables = EAWS_def;
-        $("#sensitivity_label").text("Additional loading");
-    });
-    $("#btn3").click(function () {
-        $("#test3").val("Dolly Duck");
-    });
-});
+        //var ADAMlabels = CMAH_def;
+
+        setLabels(CMAH_def);
+
+        $("#setCMAH").click(function () {
+            ADAMlables = CMAH_def;
+            $("#sensitivity_label").text(ADAMlables.sensitivity_to_triggers.label);
+        });
+        $("#setEAWS").click(function () {
+            ADAMlables = EAWS_def;
+            $("#sensitivity_label").text("Additional loading");
+        });
+        $("#btn3").click(function () {
+            $("#test3").val("Dolly Duck");
+        });
+    }
+);
 
 /*
+
+
+ var adamdefs;
+ $.getJSON("adam.json", function (json) {
+ adamdefs = json;
+ });
+
 
  function setLabel() {
  $("#humanTrigger").text(CMAH_def.sensitivity_to_triggers.stubborn);
@@ -25,14 +128,23 @@ $(document).ready(function () {
  function loadAdam() {
  $.getJSON("adam.json", function (adam_defs) {
  //alert(adam_def.ADAM.SensitivityToTriggers.Unreactive.natural_trigger);
- $("<div>" + adam_def.ADAM.SpatialDistribution.Isolated.density+ "</div>").appendTo( "body" );
+ $("#sd").text(adam_def.ADAM.SpatialDistribution.Isolated.density);
  });
- }
+ };
 
  function setCMAH() {
  $.getJSON("adam.json", function (adam_defs) {
  $("#sensitivity_label").text(adam_defs.CMAH.sensitivity_to_triggers.label);
  });
  }
+
+
+ var adam_def = (function () {
+ var json = null;
+ $.getJSON("adam.json", function (data) {
+ json = data;
+ });
+ return json;
+ })();
 
  */
